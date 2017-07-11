d45ee7474bb568ba8f3a5f3142132d4605de3f34
Abebe <abetarch@gmail.com>
2017-07-11 14:51:34 -0500

    1stPhase

---
+++ cs425-master/src/main/java/edu/mum/cs/projects/attendance/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package edu.mum.cs.projects.attendance;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+ 
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+     
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("student").password("123").roles("STUDENT");
+        auth.inMemoryAuthentication().withUser("admin").password("123").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("faculty").password("123").roles("FACULTY");
+    }
+     
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       
+      http.authorizeRequests()
+      .antMatchers("/welcome").access("hasRole('FACULTY') or hasRole('STUDENT') or hasRole('ADMIN')")
+      .and().formLogin().loginPage("/login")
+       .usernameParameter("username").passwordParameter("password")
+       .defaultSuccessUrl("/welcome")
+        .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+    }
+}
+ 
---
+++ cs425-master/src/main/java/edu/mum/cs/projects/attendance/controller/loginController.java
@@ -0,0 +1,31 @@
+package edu.mum.cs.projects.attendance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+public class loginController {
+
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request, Model model) {
+        String errorMessage = "";
+        System.out.print("Log in is pressed");
+        if (request.getParameter("loginFailed") != null)
+        {
+            errorMessage = "Invalid login credentials";
+        }
+
+        model.addAttribute("error", errorMessage);
+        return "login";
+
+    }
+
+}
