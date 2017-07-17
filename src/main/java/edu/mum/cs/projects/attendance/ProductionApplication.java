8791a1e02a1c40b158510ed7fe45a0675b9fa22d
wogisha Benjamin <wogisha@gmail.com>
2017-07-17 14:38:59 -0500

    removed the target classes from being tracked by git

--- src/main/java/edu/mum/cs/projects/attendance/ProductionApplication.java
+++ src/main/java/edu/mum/cs/projects/attendance/ProductionApplication.java
@@ -1,14 +1,39 @@
 package edu.mum.cs.projects.attendance;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
+
+import edu.mum.cs.projects.attendance.domain.entity.Setup;
+import edu.mum.cs.projects.attendance.repository.SetupRepository;
+import edu.mum.cs.projects.attendance.service.UserService;
 
 
 @SpringBootApplication
 @PropertySource("production.properties")
 public class ProductionApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ProductionApplication.class, args);
+		
+		ApplicationContext context  =	SpringApplication.run(ProductionApplication.class, args);
+		
+		SetupRepository setupRepository = context.getBean(SetupRepository.class);
+		
+		List<Setup> listSetup = setupRepository.findAll();
+		
+		if(listSetup.size() == 0){
+			
+			UserService service = context.getBean(UserService.class);
+			service.creatUsers();			
+			Setup temp = new Setup();
+			temp.setVersion("1.0.0");
+			setupRepository.save(temp);
+			
+		}
+		
+		
+		
 	}
 }
