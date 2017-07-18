03dc81c8586d9a96032f3a4f20266318c6c03c24
simaxutian <275556253@qq.com>
2017-07-18 13:54:16 -0500

    working on faculty

---
+++ src/main/java/edu/mum/cs/projects/attendance/controller/FacultyController.java
@@ -0,0 +1,44 @@
+package edu.mum.cs.projects.attendance.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.mum.cs.projects.attendance.domain.entity.CourseOffering;
+import edu.mum.cs.projects.attendance.domain.entity.Faculty;
+import edu.mum.cs.projects.attendance.domain.entity.Users;
+import edu.mum.cs.projects.attendance.repository.CourseOfferingRepository;
+import edu.mum.cs.projects.attendance.repository.FacultyRepository;
+import edu.mum.cs.projects.attendance.service.EnrollmentService;
+
+@Controller
+public class FacultyController {
+	@Autowired
+	EnrollmentService enrollmentService;
+	
+	@Autowired
+	CourseOfferingRepository courseOfferingRepository ;
+	
+	@Autowired
+	FacultyRepository facultyRepository;
+
+	@RequestMapping(value="/faculty/courseList")
+	public String facultyCourseList(Model model,Authentication authentication){
+		Users user = (Users)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		System.out.println(user);
+		Long facultyId = user.getFacultyId();
+		
+		
+		Faculty faculty = facultyRepository.findById(facultyId);
+		List<CourseOffering> courseOfferingList = courseOfferingRepository.findByFaculty(faculty);
+		
+		
+		model.addAttribute("courseOfferingList", courseOfferingList);
+		return "facultyCourseList";
+	}
+}
