8faf1652fdef79523ab4be262a489a3b5ca1ba9e
baoxianjian <baoxianjian@gmail.com>
2017-07-19 04:53:28 +0800

    test

---
+++ src/test/java/edu/mum/cs/projects/attendance/util/StudentServiceTest.java
@@ -0,0 +1,18 @@
+package edu.mum.cs.projects.attendance.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.mum.cs.projects.attendance.service.StudentService;
+import edu.mum.cs.projects.attendance.service.StudentServiceImpl;
+
+public class StudentServiceTest {
+
+	@Test
+	public void testGetStudentsById() {
+		assertTrue("error", StudentService.getInstance().getStudentsById(null)==null);
+		assertTrue("error", StudentService.getInstance().getStudentsById("000-98-0003").getId().equals("000-98-0003"));
+	}
+
+}
